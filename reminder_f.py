2f50b4beafcf0d648a0cc0cf5594fd89065c2a60
Sakshi <sakshi.garg1098@gmail.com>
2021-05-26 18:59:24 +0530

    adding comments

--- reminder_f.py
+++ reminder_f.py
@@ -1,6 +1,4 @@
-time_in_words = ['noon', 'midnight', 'in the morning', 'in the evening', 'in the afternoon']
-time_in_am_pm = ['12:00', '00:00', 'am', 'pm', 'pm']
-default_time = '12:00'
+# counting the number of numerical words in a sentence
 
 
 def count_num(sentence):
@@ -11,48 +9,60 @@ def count_num(sentence):
     return num
 
 
+# converting words in sentences into time
+
+time_in_words = ['noon', 'midnight', 'in the morning', 'in the evening', 'in the afternoon']
+time_in_am_pm = ['12:00', '00:00', 'am', 'pm', 'pm']
+
+
 def convert_time(a_string):
     for i in [0, 1, 2, 3, 4]:
         a_string = a_string.replace(time_in_words[i], time_in_am_pm[i])
     return a_string
 
 
+# counting the number of numerical words in a sentence, the one which follows 'at' is time.
+# when there is no numerical or  no time given, default time is used.
+
+default_time = '12:00'
+
+
 def notification_time(a_string):
-    not_time = ''
-    time = ''
+    time1 = ''
+    time_in_hours = ''
     for word in a_string.split():
         if word[0].isdigit():
             if count_num(a_string) == 1 and a_string.split()[a_string.split().index(word)-1] == 'at':
-                if a_string.split().index(word) == len(a_string.split())-1:
-                    not_time = word
-                else:
-                    not_time = " ".join([word, a_string.split()[a_string.split().index(word)+1]])
-            elif count_num(a_string) == 1 and a_string.split()[a_string.split().index(word) - 1] != 'at':
-                not_time = default_time
-        elif count_num(a_string) == 0:
-            not_time = default_time
-    if len(not_time.split()) > 1:
-        if not_time.split()[1] == 'pm' and int(not_time.split()[0].split(':')[0]) < 12:
-            not_time = not_time.replace(not_time.split()[0].split(':')[0], str(int(not_time.split()[0].split(':')[0])+12), 1)
-            time = not_time.split()[0]
-        elif not_time.split()[1] == 'am':
-            time = not_time.split()[0]
-    else:
-        time = not_time
-    return time
+                if a_string.split().index(word) == len(a_string.split())-1:              # for time : 12:00 or 00:00
+                    time1 = word
+                else:                       # for time : 6:30 pm, 10:00 am ..
+                    time1 = " ".join([word, a_string.split()[a_string.split().index(word)+1]])
+            elif count_num(a_string) == 1 and a_string.split()[a_string.split().index(word) - 1] != 'at':   # time is not given
+                time1 = default_time
+        elif count_num(a_string) == 0:      # time is not given
+            time1 = default_time
+    if len(time1.split()) > 1:              # converting 12 hour (am/pm) into 24 hour clock
+        if time1.split()[1] == 'pm' and int(time1.split()[0].split(':')[0]) < 12:
+            time1 = time1.replace(time1.split()[0].split(':')[0], str(int(time1.split()[0].split(':')[0])+12), 1)
+            time_in_hours = time1.split()[0]
+        elif time1.split()[1] == 'am':
+            time_in_hours = time1.split()[0]
+    else:                                   # for time like 12:00 or 00:00
+        time_in_hours = time1
+    return time_in_hours
 
 
 def notification_message(a_string):
-    if ' '.join(a_string.split()[:3]) == 'Remind me to':
-        if 'at' in a_string.split():
+    if ' '.join(a_string.split()[:3]) == 'Remind me to':                       # for reminders beginning with 'Remind me to'
+        if 'at' in a_string.split():                                           # message will be between 'to' and 'at' i.e. till time
             return ' '.join(a_string.split()[3:a_string.split().index('at')])
-        else:
+        else:                                                                  # message will be from 'to' till end
             return ' '.join(a_string.split()[3:])
-    else:
+    else:                                                                      # for reminders not beginning with 'Remind me to'
         if 'at' in a_string.split():
-            return ' '.join(a_string.split()[:a_string.split().index('at')])
+            return ' '.join(a_string.split()[:a_string.split().index('at')])   # message will be 'at' i.e. till time
         else:
-            return a_string
+            return a_string                                                    # message will be the reminder itself
 
 
 with open("C:/Users/Acer/Desktop/reminder_data.txt", "r") as data:
